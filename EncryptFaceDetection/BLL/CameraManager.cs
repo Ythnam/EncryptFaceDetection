96b336561f8f8f8a150596f54b6f657dcdfc0866
Ythnam <phildesalondepce@hotmail.fr>
2018-05-31 12:56:41 +0200

    Add CameraManager

---
+++ EncryptFaceDetection/BLL/CameraManager.cs
@@ -0,0 +1,97 @@
+﻿using AForge.Video.DirectShow;
+using EncryptFaceDetection.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptFaceDetection.BLL
+{
+    class CameraManager
+    {
+
+        #region Internal datas
+
+        private CameraModel currentCamera;
+
+        #endregion
+
+        #region Properties
+
+        public event NewCameraFrameEvent New_Frame; // Allows to send the current frame to the ViewModel
+
+        public bool IsCameraActivated { get; set; }  // Allows to know if camera is Running but needn't camera instanciation
+
+        #endregion
+
+        #region External Function
+
+        /// <summary>
+        /// Allow to Start a camera
+        /// </summary>
+        /// <param name="_camera"> Camera we want to start </param>
+        public void StartCamera(CameraModel _camera)
+        {
+            if (_camera.CurrentDevice != null)
+            {
+                _camera.VideoSource = new VideoCaptureDevice(_camera.CurrentDevice.MonikerString);
+                _camera.VideoSource.NewFrame += video_NewFrame;
+                _camera.VideoSource.Start();
+
+                this.currentCamera = _camera; // Save current camera
+
+                this.IsCameraActivated = true;
+            }
+        }
+
+        /// <summary>
+        /// Allows to Stop the current camera
+        /// </summary>
+        public void StopCamera()
+        {
+            if (this.currentCamera.VideoSource != null && this.currentCamera.VideoSource.IsRunning)
+            {
+                this.currentCamera.VideoSource.SignalToStop();
+                this.currentCamera.VideoSource.NewFrame -= video_NewFrame;
+                this.IsCameraActivated = false;
+            }
+        }
+
+        /// <summary>
+        /// Allows to open Camera properties (it's a Aforge function that allows this)
+        /// </summary>
+        public void ShowCameraProperties(CameraModel camera)
+        {
+            if (this.IsCameraActivated)
+                camera.VideoSource.DisplayPropertyPage(IntPtr.Zero);
+        }
+
+        public void SaveImage(Bitmap bitmap)
+        {
+            BitmapHelper.SaveImage(bitmap);
+        }
+        #endregion
+
+        #region Event
+
+        public void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            try
+            {
+                using (var bitmap = (Bitmap)eventArgs.Frame.Clone())
+                {
+                    if (this.New_Frame != null)
+                        New_Frame(this, new NewCameraFrameEventArgs(bitmap));
+                }
+            }
+            catch (VideoException exc)
+            {
+                StopCamera();
+                Console.WriteLine(exc);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ EncryptFaceDetection/Events/NewCameraFrameEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptFaceDetection.Events
+{
+    public delegate void NewCameraFrameEvent(object source, NewCameraFrameEventArgs e);
+
+    public class NewCameraFrameEventArgs : EventArgs
+    {
+        private readonly Bitmap EventBitmap;
+        public NewCameraFrameEventArgs(Bitmap _bitmap)
+        {
+            EventBitmap = _bitmap;
+        }
+        public Bitmap GetBitmap()
+        {
+            return EventBitmap;
+        }
+    }
+}
